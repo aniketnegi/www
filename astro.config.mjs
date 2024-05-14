1bf67275183724624a61151babcdf57fd5592ce8
aniket negi <aniket.negi19104@gmail.com>
2024-05-14 21:41:55 +0530

    mobile nav works

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import tunnel from "astro-tunnel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), tunnel()],
   site: "https://aniketnegi.vercel.app",
-  trailingSlash: 'always',
+  trailingSlash: 'always'
 });
---
+++ src/utils/breadcrumbs.ts
@@ -0,0 +1,7 @@
+export const breadcrumbsTitles = {
+    "blog": "BLOG",
+    "garden": "GARDEN",
+    "lit": "LITERATURE",
+    "projects": "PROJECTS",
+    // TBD
+}
