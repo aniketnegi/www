1e9572c9790023d49f44103144e4b06c30a529b6
aniket negi <aniket.negi19104@gmail.com>
2024-05-17 23:53:16 +0530

    added compression lib @playform/compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import tunnel from "astro-tunnel";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), tunnel()],
+  integrations: [tailwind(), icon(), tunnel(), playformCompress()],
   site: "https://aniketnegi.vercel.app",
   trailingSlash: 'always'
 });
