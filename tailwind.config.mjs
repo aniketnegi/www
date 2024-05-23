8b04e60ab001ad29c855e2ad03dd57f105e0fe53
aniket negi <aniket.negi19104@gmail.com>
2024-05-23 20:03:22 +0530

    changed styles blog

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -30,7 +30,12 @@ export default {
 			"sans": ["Manrope Variable", "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
 			"serif": ["Newsreader Variable", "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
 			"mono": ["JetBrains Mono Variable", "ui-monospace", "SFMono-Regular", "Menlo", "Monaco", "Consolas", "Liberation Mono", "Courier New", "monospace"],
-			"hi": ["Noto Serif Devanagari Variable"],
+
+			// my site's payload is 60% fonts. i should just do this instead
+			// "sans": ["ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+			// "serif": ["ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
+			// "mono": ["ui-monospace", "SFMono-Regular", "Menlo", "Monaco", "Consolas", "Liberation Mono", "Courier New", "monospace"],
+			// "hi": ["Noto Serif Devanagari Variable"],
 		},
 
 		extend: {
@@ -41,6 +46,42 @@ export default {
 				DEFALUT: {
 					css:
 					{
+						// Colours
+						'--tw-prose-body': theme('colors.LM-Text'),
+						'--tw-prose-headings': theme('colors.LM-Text'),
+						'--tw-prose-lead': theme('colors.LM-Text'),
+						'--tw-prose-links': theme('colors.LM-Text'),
+						'--tw-prose-bold': theme('colors.LM-Text'),
+						'--tw-prose-counters': theme('colors.LM-Text'),
+						'--tw-prose-bullets': theme('colors.LM-Text'),
+						'--tw-prose-hr': theme('colors.LM-Text'),
+						'--tw-prose-quotes': theme('colors.LM-Text'),
+						'--tw-prose-quote-borders': theme('colors.LM-Text'),
+						'--tw-prose-captions': theme('colors.LM-Text'),
+						'--tw-prose-kbd': theme('colors.LM-Text'),
+						'--tw-prose-kbd-shadows': theme('colors.LM-Text'),
+						'--tw-prose-code': theme('colors.LM-Text'),
+						'--tw-prose-pre-code': theme('colors.LM-Text'),
+						'--tw-prose-pre-bg': theme('colors.LM-Text'),
+						'--tw-prose-th-borders': theme('colors.LM-Text'),
+						'--tw-prose-td-borders': theme('colors.LM-Text'),
+						'--tw-prose-invert-body': theme('colors.DM-Text'),
+						'--tw-prose-invert-headings': theme('colors.DM-Text'),
+						'--tw-prose-invert-lead': theme('colors.DM-Text'),
+						'--tw-prose-invert-links': theme('colors.DM-Text'),
+						'--tw-prose-invert-bold': theme('colors.DM-Text'),
+						'--tw-prose-invert-counters': theme('colors.DM-Text'),
+						'--tw-prose-invert-bullets': theme('colors.DM-Text'),
+						'--tw-prose-invert-hr': theme('colors.DM-Text'),
+						'--tw-prose-invert-quotes': theme('colors.DM-Text'),
+						'--tw-prose-invert-quote-borders': theme('colors.DM-Text'),
+						'--tw-prose-invert-captions': theme('colors.DM-Text'),
+						'--tw-prose-invert-kbd': theme('colors.DM-Text'),
+						'--tw-prose-invert-kbd-shadows': theme('colors.DM-Text'),
+						'--tw-prose-invert-code': theme('colors.DM-Text'),
+						'--tw-prose-invert-pre-code': theme('colors.DM-Text'),
+						'--tw-prose-invert-th-borders': theme('colors.DM-Text'),
+						'--tw-prose-invert-td-borders': theme('colors.DM-Text'),
 					}
 
 				}
