776e41c9f20446f60cfda63d76264576f0429072
aniket negi <aniket.negi19104@gmail.com>
2024-05-27 11:15:17 +0530

    changed defer in umami script

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import tunnel from "astro-tunnel";
 import playformCompress from "@playform/compress";
-
 import mdx from "@astrojs/mdx";
 import remarkToc from 'remark-toc';
 import remarkObsidian from "remark-obsidian";
 import rehypeExternalLinks from 'rehype-external-links';
-
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,18 +14,16 @@ export default defineConfig({
     syntaxHighlight: 'shiki',
     shikiConfig: {
       theme: "houston",
-      defaultColor: false,
-
+      defaultColor: false
     },
     remarkPlugins: [remarkToc, remarkObsidian],
-    rehypePlugins: [[
-      rehypeExternalLinks,
+    rehypePlugins: [[rehypeExternalLinks
       // {
       //   content: { type: 'svg', value: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-arrow-out-up-right"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6"/><path d="m21 3-9 9"/><path d="M15 3h6v6"/></svg>' }
       // }
-    ],],
-    gfm: false,
-  }),],
+    ]],
+    gfm: false
+  })],
   site: "https://www.aniketnegi.com/",
   trailingSlash: 'always'
 });
